ad3103355d10d26ed9073a971578f939d7a5829a
Marlin Gingerich <marlin.gingerich@gmail.com>
2020-06-01 16:30:31 -0400

    update siteUrl

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteTitle: 'Jacquie | Piano Teacher', // Site title.
   siteTitleAlt: 'Piano Teacher', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://jacqueline-piano.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'https://jacquieteaches.ca', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
   siteDescription: 'Music lessons in KW area offered by Jacquie.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'jacqueline-piano', // Disqus shortname.
+  disqusShortname: 'jacquie-teaches', // Disqus shortname.
   userName: 'Jacquie George',
   // userTwitter: 'vaibhaved',
   userLocation: 'Kitchener, ON',
